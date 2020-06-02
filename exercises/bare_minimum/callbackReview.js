cb3d8eb93fed6954f67eaa952161f1c4536dbd98
Rita Rozsavolgyi <rvrita@Ritas-Air.attlocal.net>
2020-06-01 22:55:33 -0700

    WIP

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -7,7 +7,18 @@ var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+  var counter = 0;
+  var acc = '';
+  request.on('data', (chunk) => {
+    acc += chunk;
+    counter++;
+    if (counter > 0) {
+      request.end(acc);
+    }
+  });
+  request.on('end', (data) => {
+    fs.readFile();
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
